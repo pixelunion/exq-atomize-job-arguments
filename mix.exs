6c9a91e0111ae7e1cb619f3963b5539e790d2c5c
Daniel Insley <dan@insley.ca>
2019-01-30 14:42:22 -0800

    version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule ExqAtomizeJobArguments.MixProject do
   def project do
     [
       app: :exq_atomize_job_arguments,
-      version: "0.1.1",
+      version: "0.1.2",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
       deps: deps()
